306256d004c2988134e1125eb611cfa8dfc353a3
mdejean <marcel.dejean@alumni.case.edu>
2016-12-04 00:15:48 -0500

    SpaceWare as IMPORTED target, test .lib on Windows
    Creates an IMPORTED target for the SpaceWare library so that
    src/CMakeFiles.txt only has to mention it in target_link_library.
    
    Also try_compile with the .lib on Windows so that if the static library is
    not compatible (e.g. win64) it is disabled.

--- cmake/FindSpaceWare.cmake
+++ cmake/FindSpaceWare.cmake
@@ -4,7 +4,7 @@
 #
 # SPACEWARE_INCLUDE_DIR - header location
 # SPACEWARE_LIBRARIES - library to link against
-# SPACEWARE_FOUND - true if pugixml was found.
+# SPACEWARE_FOUND - true if spaceware was found.
 
 if(UNIX)
 
@@ -14,15 +14,54 @@ if(UNIX)
     find_library(SPACEWARE_LIBRARY
         NAMES spnav libspnav)
 
-    # Support the REQUIRED and QUIET arguments, and set SPACEWARE_FOUND if found.
-    include(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(SPACEWARE DEFAULT_MSG
-        SPACEWARE_LIBRARY SPACEWARE_INCLUDE_DIR)
+elseif(WIN32)
 
-    if(SPACEWARE_FOUND)
-        set(SPACEWARE_LIBRARIES ${SPACEWARE_LIBRARY})
+    find_path(SPACEWARE_INCLUDE_DIR
+        si.h
+        PATHS ${CMAKE_SOURCE_DIR}/extlib/si)
+
+    find_library(SPACEWARE_LIBRARY
+        NAMES siapp
+        PATHS ${CMAKE_SOURCE_DIR}/extlib/si)
+
+    if (MSVC)
+        set(SPACEWARE_LINK_FLAGS /SAFESEH:NO)
     endif()
 
-    mark_as_advanced(SPACEWARE_LIBRARY SPACEWARE_INCLUDE_DIR)
-
 endif()
+
+# Support the REQUIRED and QUIET arguments, and set SPACEWARE_FOUND if found.
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SPACEWARE DEFAULT_MSG
+    SPACEWARE_LIBRARY SPACEWARE_INCLUDE_DIR)
+
+if(WIN32)
+    # Test that the library links correctly
+    set(SPACEWARE_TEST_FILE ${CMAKE_BINARY_DIR}/CMakeTmp/sitest.c)
+    file(WRITE ${SPACEWARE_TEST_FILE}
+        "#include <si.h>\n"
+        "#include <siapp.h>\n"
+        "int main(void) {SiInitialize();SiTerminate();return 0;}")
+    try_compile(SPACEWARE_FOUND ${CMAKE_BINARY_DIR}/CMakeTmp/sitest
+        SOURCES ${SPACEWARE_TEST_FILE}
+        CMAKE_FLAGS "-DEXE_LINKER_FLAGS=${SPACEWARE_LINK_FLAGS}"
+            "-DLINK_LIBRARIES=${SPACEWARE_LIBRARY}"
+            "-DINCLUDE_DIRECTORIES=${SPACEWARE_INCLUDE_DIR}")
+    if (SPACEWARE_FOUND)
+        message("Using prebuilt SpaceWare")
+    else()
+        message("Not using prebuilt SpaceWare (incompatible library)")
+    endif()
+endif()
+
+if(SPACEWARE_FOUND)
+    set(SPACEWARE_LIBRARIES ${SPACEWARE_LIBRARY})
+    add_library(SpaceWare STATIC IMPORTED)
+    set_target_properties(SpaceWare PROPERTIES
+        IMPORTED_LOCATION ${SPACEWARE_LIBRARY}
+        INTERFACE_INCLUDE_DIRECTORIES ${SPACEWARE_INCLUDE_DIR})
+    #INTERFACE_LINK_FLAGS doesn't exist
+    set(CMAKE_EXE_LINKER_FLAGS   "${CMAKE_EXE_LINKER_FLAGS} ${SPACEWARE_LINK_FLAGS}")
+endif()
+
+mark_as_advanced(SPACEWARE_LIBRARY SPACEWARE_INCLUDE_DIR)
